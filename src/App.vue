9bf5b466a96c5944fbb38a800d9f1845e40e8bee
LoMax48 <lomax4848@gmail.com>
2021-10-24 16:01:38 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ConverterComponent name="Конвертер миль в километры" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConverterComponent from "@/components/ConverterComponent";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ConverterComponent,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ConverterComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <h1>{{ name }}</h1>
+        <div class="input-group mb-3 text-center">
+            <input type="text" class="form-control" placeholder="Мили" v-model="sourceValue">
+            <span class="input-group-text">→</span>
+            <input type="text" class="form-control" placeholder="Километры" :value="convertMilesToKm">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ConverterComponent",
+    props: {
+        name: String,
+    },
+    data() {
+        return {
+            sourceValue: null,
+        }
+    },
+    computed: {
+        convertMilesToKm() {
+            if (this.sourceValue == null || isNaN(this.sourceValue)) {
+                return;
+            } else {
+                return this.sourceValue * 1.609;
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
